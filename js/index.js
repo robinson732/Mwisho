1ac98f59cbdb61fc6e55a1d55e0130908c10523d
Robinson Kimani <kimanirobinson336@gmail.com>
2025-02-05 20:36:43 +0300

    Initial js coding

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const quotes = [
+    {
+        text : "If you think you do not understand then you are lacking",
+        author: "King Von"
+    }
+    {
+        text : "The river between is not the river underneath",
+        author: "Robinson Chege"
+
+    }
+]
