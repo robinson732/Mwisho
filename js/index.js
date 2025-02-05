f943dfe9dbf9f50cb657e127e240055cf8f81905
Robinson Kimani <kimanirobinson336@gmail.com>
2025-02-05 20:52:32 +0300

    More quotes

--- js/index.js
+++ js/index.js
@@ -2,10 +2,54 @@ const quotes = [
     {
         text : "If you think you do not understand then you are lacking",
         author: "King Von"
-    }
+    },
     {
         text : "The river between is not the river underneath",
         author: "Robinson Chege"
-
+    },
+    {
+        text : "The man who forgets that his wife cannot take a joke forgets that his wife took him",
+        author : "Oscar Wilde"
+    },
+    {
+        text: "If you change your mind you can change your life",
+        author :"Walt Whitman"
+    },
+    {
+        text: "If you change the way you look at thingsd the things you look at change",
+        author : "Wayne Dyer"
+    },
+    {
+        text : "Try not to become a man of success rather be a man of value",
+        author : "Albert Einstein"
+    },
+    {
+        text : "It is never too late to be what you might have been",
+        author : "George Elliot"
+    },
+    {
+        text : "The biggest adventure you can ever take is to live the life of your dreams",
+        author : "Oprah Winfrey"
     }
+    {
+        text : "We only live once",
+        author : "Gabriel Mochire"
+    },
+    {
+        text : "Nani alisema haiwezekani",
+        author : "Jonathan Njoroge"
+    },
+    {
+        text : "The only thing we have to fear is fear itself",
+        author : "Franklin Roosevelt"
+    },
+    {
+        text : "Perseverance goes a long way",
+        author : "Winston Churchill"
+    },
+    {
+        text : "Dont let your circumstances stop you",
+        author : "Mark Caine"
+    },
+
 ]
